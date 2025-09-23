ded652dd1a590ddc0069cafd88a7e27d75e37763
Mehajabin36 <shabnamahin67@gmail.com>
2025-09-23 15:00:09 +0530

    Delete python directory

--- python/natural.py
+++
@@ -1,3 +0,0 @@
-print("SUM OF FIRST 10 NATURAL NUMBERS")
-num=[1,2,3,4,5,6,7,8,9,10]
-print(sum(num))
--- python/ordinal.py
+++
@@ -1,4 +0,0 @@
-print("ORDINAL VALUES OF A WORD")
-word=input("enter the word:")
-value=[ord(ch) for ch in word]
-print("ordinal values:",value)
