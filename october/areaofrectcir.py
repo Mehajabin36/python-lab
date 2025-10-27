a5c7e640455054c54d6fcc3b05cfd0813163c239
Mehajabin36 <shabnamahin67@gmail.com>
2025-10-27 16:26:54 +0530

    Add files via upload

---
+++ october/areaofrectcir.py
@@ -0,0 +1,8 @@
+def rectangle
+a=float(input("enter the length:\n")
+b=float(input("enter the breadth:\n")
+area=a*b
+
+def circle
+r=float(input("enter the radius of the circle:"))
+print("area of the circle=",3.14*r*r)        
---
+++ october/fiboutput.py
@@ -0,0 +1,2 @@
+import fiboimport
+fiboimport.fibonacci
